1d43f03c8a66919424bde1caa57108f84d2bddf6
João Lucas Sousa Reis <joao.lucas.ssr@gmail.com>
2018-06-03 17:11:45 -0300

    Added contracts

--- diskspace/diskspace.py
+++ diskspace/diskspace.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import print_function
+from contracts import contract
 
 import argparse
 import os
@@ -35,11 +36,11 @@ args = parser.parse_args()
 
 
 # ==== Disk Space ====
-
+@contract(command='string', returns='string')
 def subprocess_check_output(command):
     return subprocess.check_output(command.strip().split(' '))
 
-
+@contract(blocks='int, > 0', returns='string')
 def bytes_to_readable(blocks):
     byts = blocks * 512
     readable_bytes = byts
@@ -51,7 +52,7 @@ def bytes_to_readable(blocks):
     labels = ['B', 'Kb', 'Mb', 'Gb', 'Tb']
     return '{:.2f}{}'.format(round(byts/(1024.0**count), 2), labels[count])
 
-
+@contract(file_tree='dict', file_tree_node='dict', path='string', largest_size='int, >=6', total_size='int, >=4', depth='int, >=0', returns='NoneType')
 def print_tree(file_tree, file_tree_node, path, largest_size, total_size,
                depth=0):
     percentage = int(file_tree_node['size'] / float(total_size) * 100)
@@ -71,7 +72,7 @@ def print_tree(file_tree, file_tree_node, path, largest_size, total_size,
             print_tree(file_tree, file_tree[child], child, largest_size,
                        total_size, depth + 1)
 
-
+@contract(directory='string', depth='int, >=-1', order='bool')
 def show_space_list(directory='.', depth=-1, order=True):
     abs_directory = os.path.abspath(directory)
 
